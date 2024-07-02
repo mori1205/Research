921c242b46d7b8a4a829873c198a47cc90c14eeb
mori1205 <mm2384mm@gm.tsuda.ac.jp>
2024-07-02 12:28:55 +0900

    モーター動いた！

--- Assets/MyScript/LifeManeger.cs
+++ Assets/MyScript/LifeManeger.cs
@@ -32,7 +32,7 @@ public class LifeManager : MonoBehaviour
 
   IEnumerator InvincibilityTime()
   {
-    yield return new WaitForSeconds(5f);
+    yield return new WaitForSeconds(2f);
     obstacleHit = false;
   }
 
